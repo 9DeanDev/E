46827883323b8ee903152c25b8a0d91220c45353
Phước Phú <phuocphu414141@gmail.com>
2023-10-21 12:10:48 +0700

    update

--- src/views/LoginPage.tsx
+++ src/views/LoginPage.tsx
@@ -4,9 +4,47 @@ import eatMe from '../assets/02.png'
 import { Link } from 'react-router-dom'
 import { FaFacebookSquare } from 'react-icons/fa';
 import { FcGoogle } from 'react-icons/fc';
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 type Props = {}
 
+const schema = yup
+    .object()
+    .shape({
+        email: yup.string().email().required(),
+        username: yup.string().required(),
+        password: yup.string().required()
+    })
+    .required();
+interface IFormInput {
+    email: string;
+    username: string;
+    password: string;
+}
 const LoginPage = (props: Props) => {
+
+    // let [status, setStatus] = useState('off')
+    // const handleClickLogin = () => {
+
+    // setStatus('on')
+    // setTimeout(() => {
+    //     if (errors.email) {
+    //         setStatus('off')
+    //         alert('Sign up success')
+    //     }
+    //     else {
+    //         setStatus('off')
+    //         alert('email is required')
+    //     }
+    // }, 3000);
+    // }
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<IFormInput>({ resolver: yupResolver(schema) });
+    console.log('errors:', errors)
     return (
         <div className={Styles.container}>
             <div className={Styles.eatMe}>
@@ -20,22 +58,34 @@ const LoginPage = (props: Props) => {
                     Create an account to continue!
                 </p>
             </div>
-            <form>
+            <form onSubmit={handleSubmit((data) => console.log(data))} action='/user'>
                 <div className={Styles.formLogin}>
-                    <label>
+                    <label htmlFor='email'>
                         Email
                     </label>
-                    <input type="email" />
+                    <input id='email' type="email" {...register('email')} />
+                    <span>{errors.email?.message}</span>
                     <label>
-                        Username
+                        UserName
                     </label>
-                    <input type="text" />
+                    <input type="text" {...register('username')} />
+                    {errors.username && <p>UserName is required</p>}
                     <label>
                         Password
                     </label>
-                    <input type="password" />
+                    <input type="password" {...register('password')} />
+                    {errors.password && <p>Password is required</p>}
+                    <button type='submit'
+                    // onClick={() => handleClickLogin()}
+                    >Sign up
+                        {/* <span style={{ display: status === 'on' ? 'block' : 'none' }}> (Loading...)</span> */}
+                        {/* <Link to='/user' className={Styles.subtn}>
+                            Sign up
+                        </Link> */}
+                    </button>
                     <div className={Styles.btn}>
-                        <Link to='/user' className={Styles.subtn}>
+                        {/* <button type='submit' /> */}
+                        <Link to='/user' className={Styles.subtn} type='submit'>
                             Sign up
                         </Link>
                         <div>
--- src/views/UserPage.tsx
+++ src/views/UserPage.tsx
@@ -1,40 +1,91 @@
 import Styles from './UserPage.module.scss'
-import avatar from '../assets/avataruser.jpg'
+import avatar from '../assets/avataruser.png'
+import food1 from '../assets/the-ultimate-hamburger.jpg'
+import { FaBars } from 'react-icons/fa';
+import { BsSearch } from 'react-icons/bs';
+import { BiArrowToTop } from 'react-icons/bi';
+import { TbHeartSearch, TbShoppingCartSearch } from 'react-icons/tb';
+import { FcLike } from 'react-icons/fc';
+import { BsFillBellFill } from 'react-icons/bs';
 type Props = {}
 
 const UserPage = (props: Props) => {
     return (
         <div className={Styles.container}>
-            <div>
-                <div>
-
+            <div className={Styles.avatarContainer}>
+                <div className={Styles.btn}>
+                    <FaBars />
                 </div>
-                <img src={avatar} />
+                <div className={Styles.avatar}>
+                    <img src={avatar} />
+                </div>
             </div>
-            <div>
-                <div>
+            <div className={Styles.titleContainer}>
+                <div className={Styles.title}>
                     Let's eat
                 </div>
-                <div>
+                <div className={Styles.subTitle}>
                     Nutrious food
                 </div>
             </div>
-            <div>
-                <input />
+            <div className={Styles.searchBar}>
+                <BsSearch />
+                <input className={Styles.search} placeholder='Find your food here...' />
             </div>
-            <div>
-                <div>
+            <div className={Styles.navFood}>
+                <div className={Styles.navFoodChild}>
                     Foods
                 </div>
+                <div className={Styles.navFoodChild}>
+                    Fruits
+                </div>
+                <div className={Styles.navFoodChild}>
+                    Juices
+                </div>
+                <div className={Styles.navFoodChild}>
+                    Vegetables
+                </div>
             </div>
-            <div>
-
+            <div className={Styles.rpContainer}>
+                <div className={Styles.title}>
+                    Recommended Products
+                </div>
+                <div className={Styles.seeAll}>
+                    See all
+                </div>
             </div>
-            <div>
+            <div className={Styles.foodContainer}>
+                <div className={Styles.foodChildContainer}>
+                    <div className={Styles.foodImg}>
+                        <img src={food1} />
+                    </div>
+                    <div className={Styles.foodDetails}>
+                        <div className={Styles.foodNote}>
+                            Drinks/Beverages
 
+                        </div>
+                        <div className={Styles.foodPrice}>
+                            $7.38
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-
+            <div className={Styles.footer}>
+                <div>
+                    <BiArrowToTop />
+                </div>
+                <div>
+                    <TbHeartSearch />
+                </div>
+                <div>
+                    <TbShoppingCartSearch />
+                </div>
+                <div>
+                    <FcLike />
+                </div>
+                <div>
+                    <BsFillBellFill />
+                </div>
             </div>
         </div>
     )
